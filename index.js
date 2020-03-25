be21bfe635760e4ca021804ce653fbd6673eac6d
John Loberger <johnloberger@gmail.com>
2020-03-25 02:10:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ function doToElementsInArray(array, callback){
   array.forEach(callback);
 }
 var animals = ["dog", "cat", "squirrel"]
+function changeCompletely(element, index, array){
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`
+}
 doToElementsInArray (animals, changeCompletely)
 // Add your changeCompletely() function here:
