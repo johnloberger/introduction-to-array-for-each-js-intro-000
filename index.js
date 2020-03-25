58afa3551ac68c6aabf057f8304f02e7c6d6f057
John Loberger <johnloberger@gmail.com>
2020-03-25 02:08:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Add your doToElementsInArray() function here:
-
-
+function doToElementsInArray(array, callback){
+  array.forEach(callback);
+}
+var animals = ["dog", "cat", "squirrel"]
+doToElementsInArray (animals, changeCompletely)
 // Add your changeCompletely() function here:
